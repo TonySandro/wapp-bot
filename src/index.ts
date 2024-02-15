66fe07f4302d84edb5bd272c09cf6084bda4caf3
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2024-02-15 17:08:44 -0300

    feat: add whatsapp-web.js connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("test")
+import qrcode from 'qrcode-terminal'
+import { Client, LocalAuth } from "whatsapp-web.js";
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true})
+})
+
+client.on('ready', () => {
+    console.log('Client is ready!')
+})
+
+client.on('message', async (message) => {
+	if (message.body === '!ping') {
+		await message.reply('pong');
+	}
+});
+
+client.initialize()
